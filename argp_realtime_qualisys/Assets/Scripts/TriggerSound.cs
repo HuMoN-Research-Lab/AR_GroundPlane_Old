294506287f58fba99748acda71e6c5ac61fb9a5c
TrentWirth <trentondwirth@gmail.com>
2022-03-21 17:16:45 -0400

    3 projectors & threshold testing
    Set up 3 different projectors/cameras in Unity, achieved accurate collision detection between L-Bracket and targets in all three views. Began testing with velocity thresholds; still figuring that part out. We successfully wrote code that detects the velocity in the Z (length of the lab, Y) direction, but we need to figure out what the best threshold for detecting a foot step will be. That, on Thursday!

--- argp_realtime_qualisys/Assets/Scripts/TriggerSound.cs
+++ argp_realtime_qualisys/Assets/Scripts/TriggerSound.cs
@@ -6,19 +6,30 @@ public class TriggerSound : MonoBehaviour
 {
     // reference to the AudioClip we want to play on trigger enter.
     public AudioClip soundTrigger;
+    private float velocity; 
+    private float previous;
+
 
     /// OnTriggerEnter is called when the Collider 'other' enters the trigger.
     void OnTriggerEnter(Collider other)
     {
-        if (other.name == "L-Frame O")
+        velocity = (other.GetComponent<Rigidbody>().position.z - previous) / Time.deltaTime;
+        previous = other.GetComponent<Rigidbody>().position.z;
+
+        float this_velocity = Mathf.Sqrt(Mathf.Pow(velocity,2f));
+         
+
+        if (this_velocity < 1)
         {
             // disable the whole GameObject
             gameObject.SetActive(false);
 
             Debug.Log("Collision!");
+            Debug.Log(this_velocity);
 
             // play the collect sound (at the same position as the target, 100% volume)
             AudioSource.PlayClipAtPoint(soundTrigger, transform.position, 1.0f);
         }
     }
 }
+
